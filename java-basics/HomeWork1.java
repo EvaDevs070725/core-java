473f254f1f4501f50593d366915b79cfa06adaf7
Jbkpar <jbkpar@gmail.com>
2025-07-14 16:41:01 -0400

    completed homework

---
+++ java-basics/HomeWork1.java
@@ -0,0 +1,10 @@
+
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        int arg1 = Integer.parseInt(args[0]), arg2 = Integer.parseInt(args[1]);
+        System.out.printf("addition results %d \n", arg1 + arg2);
+        System.out.printf("substraction results %d \n", arg1 - arg2);
+        System.out.printf("multiplication results %d \n", arg1 * arg2);
+    }
+}
